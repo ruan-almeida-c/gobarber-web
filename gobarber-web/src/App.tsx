3125b8da06b3c6bb1c50137a446d9af5f17f09a4
Ruan <ru_almeida@id.uff.br>
2020-09-24 02:18:11 -0300

    First Commit

---
+++ gobarber-web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+import SingIn from './pages/sigIn/index';
+
+const App: React.FC = () => (
+  <>
+    <SingIn />
+    <GlobalStyle />
+  </>
+
+);
+
+export default App;
