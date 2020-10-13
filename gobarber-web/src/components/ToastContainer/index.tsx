7d412bf3a85bc82fdde138b130204ca94169cdd1
Ruan <ru_almeida@id.uff.br>
2020-10-13 01:44:09 -0300

    add Animation in Toast

--- gobarber-web/src/components/ToastContainer/index.tsx
+++ gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,48 +1,31 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { Container, Toast } from './styles';
+import { useTransition } from 'react-spring';
+import Toast from './Toast';
+import { ToastMessage } from '../../hooks/toast';
+import { Container } from './styles';
 
-const ToastContainer: React.FC = () => (
-  <Container>
-    <Toast hasDescription>
-      <FiAlertCircle size={20} />
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
 
-      <div>
-        <strong>Aconteceu um erro</strong>
-        <p>Não foi possível fazer login na aplicação</p>
-      </div>
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransition = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
 
-      <button type="button">
-        <FiXCircle size={18} />
-      </button>
-    </Toast>
-
-    <Toast type="success" hasDescription={false}>
-      <FiAlertCircle size={20} />
-
-      <div>
-        <strong>Aconteceu um erro</strong>
-      </div>
-
-      <button type="button">
-        <FiXCircle size={18} />
-      </button>
-    </Toast>
-
-    <Toast type="error" hasDescription>
-      <FiAlertCircle size={20} />
-
-      <div>
-        <strong>Aconteceu um erro</strong>
-        <p>Não foi possível fazer login na aplicação</p>
-      </div>
-
-      <button type="button">
-        <FiXCircle size={18} />
-      </button>
-    </Toast>
-  </Container>
-
-);
+  return (
+    <Container>
+      {messagesWithTransition.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
+      ))}
+    </Container>
+  );
+};
 export default ToastContainer;
